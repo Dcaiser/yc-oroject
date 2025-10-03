621c86d9a814102af6e39c7f0dffd415225eff00
danaaraga <danaraga101@gmail.com>
2025-10-03 13:43:49 +0700

    Fix: Correct AppServiceProvider class definition

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,29 +2,23 @@
 
 namespace App\Providers;
 
-use App\Models\User;
-use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
 
-class AuthServiceProvider extends ServiceProvider
+class AppServiceProvider extends ServiceProvider
 {
-    protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
-    ];
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
-        // Define Gates
-        Gate::define('admin', function (User $user) {
-            return $user->isAdmin();
-        });
-
-        Gate::define('manager', function (User $user) {
-            return $user->isManager() || $user->isAdmin();
-        });
-
-        Gate::define('staff', function (User $user) {
-            return $user->isStaff() || $user->isManager() || $user->isAdmin();
-        });
+        //
     }
 }
